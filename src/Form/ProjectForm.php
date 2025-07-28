91bfda27cbd212f51acf88cf28926f62e2338ae9
Anthony Delbee <anthony.delbee@gmail.com>
2025-07-28 22:33:43 +0200

    Fix erreur double menu actif + texte en anglais

--- src/Form/ProjectForm.php
+++ src/Form/ProjectForm.php
@@ -20,17 +20,23 @@ class ProjectForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', TextType::class)
+            ->add('title', TextType::class,[
+                'label' => 'Titre',
+                'required' => true,
+            ])
             ->add('customer', EntityType::class, [
+                'label' => 'Client',
                 'class' => Customer::class,
                 'choice_label' => 'name',
             ])
             ->add('manager', EntityType::class, [
+                'label' => 'Responsable',
                 'class' => Manager::class,
                 'choice_label' => 'name',
                 'required' => false,
             ])
             ->add('deadline', DateType::class, [
+                'label' => 'Date limite',
                 'widget' => 'single_text',
                 'required' => false,
             ])
